c601e40fcbb906ec346b6a23da52ce792c896c7e
kevingermainbusiness <kevingermainbusiness@gmail.com>
2021-04-27 22:06:52 -0400

    Second commit

--- app/src/main/java/com/kevincodes/androidcanvas/MainActivity.kt
+++ app/src/main/java/com/kevincodes/androidcanvas/MainActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import com.kevincodes.androidcanvas.databinding.ActivityMainBinding
+import timber.log.Timber
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +19,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var mCanvas: Canvas
-    private var mRectanglePaint = Paint()
-    private var mUnderlinedPaintedText = Paint(Paint.UNDERLINE_TEXT_FLAG)
+    private var mDefaultPainter = Paint()
+    private var mUnderlinedTextPainter = Paint(Paint.UNDERLINE_TEXT_FLAG)
     private lateinit var mBitmapOnImageView: Bitmap
     private var mRect: Rect = Rect()
     private var mBounds: Rect = Rect()
@@ -43,9 +44,9 @@ class MainActivity : AppCompatActivity() {
         mColorAccent = ResourcesCompat.getColor(resources, R.color.teal_200, null)
         mWhiteColor = ResourcesCompat.getColor(resources, R.color.white, null)
         // assign colors
-        mRectanglePaint.color = mFirstDrawnBackgroundColor
-        mUnderlinedPaintedText.color = mWhiteColor
-        mUnderlinedPaintedText.textSize = 70f
+        mDefaultPainter.color = mFirstDrawnBackgroundColor
+        mUnderlinedTextPainter.color = mWhiteColor
+        mUnderlinedTextPainter.textSize = 70f
     }
 
     fun drawSomething(view: View) {
@@ -58,33 +59,41 @@ class MainActivity : AppCompatActivity() {
             binding.myImageview.setImageBitmap(mBitmapOnImageView)
             mCanvas = Canvas(mBitmapOnImageView)
             mCanvas.drawColor(mFirstDrawnBackgroundColor)
-            mCanvas.drawText(getString(R.string.keep_tapping), 100f, 100f, mUnderlinedPaintedText)
+            mCanvas.drawText(
+                getString(R.string.keep_tapping),
+                (halfWidth * 0.5).toFloat(), // 50% of half width
+                (halfHeight * 0.15).toFloat(), // 15% of half height
+                mUnderlinedTextPainter
+            )
             mOffset += OFFSET
+            Timber.d("First offset size increase: $mOffset")
         } else {
             if (mOffset < halfWidth && mOffset < halfHeight) {
                 // Change the color by subtracting an integer.
-                mRectanglePaint.color = mRectFillColor - MULTIPLIER * mOffset
+                mDefaultPainter.color = mRectFillColor - MULTIPLIER * mOffset
                 mRect.set(
                     mOffset, mOffset, vWidth - mOffset, vHeight - mOffset
                 )
-                mCanvas.drawRect(mRect, mRectanglePaint)
+                mCanvas.drawRect(mRect, mDefaultPainter)
                 // Increase the indent.
                 mOffset += OFFSET
+                Timber.d("Offset size below half image size: $mOffset")
             } else {
-                mRectanglePaint.color = mColorAccent
+                mDefaultPainter.color = mColorAccent
                 mCanvas.drawCircle(
                     halfWidth.toFloat(),
                     halfHeight.toFloat(),
                     (halfWidth / 3).toFloat(),
-                    mRectanglePaint
+                    mDefaultPainter
                 )
                 val text = getString(R.string.done)
                 // Get bounding box for text to calculate where to draw it.
-                mUnderlinedPaintedText.getTextBounds(text, 0, text.length, mBounds)
+                mUnderlinedTextPainter.getTextBounds(text, 0, text.length, mBounds)
                 // Calculate x and y for text so it's centered.
                 val x = halfWidth - mBounds.centerX()
                 val y = halfHeight - mBounds.centerY()
-                mCanvas.drawText(text, x.toFloat(), y.toFloat(), mUnderlinedPaintedText)
+                mCanvas.drawText(text, x.toFloat(), y.toFloat(), mUnderlinedTextPainter)
+                Timber.d("Offset size, not equal to original size nor inferior to half size: $mOffset")
             }
         }
         view.invalidate()
