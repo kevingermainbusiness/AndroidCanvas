78ee5cc5035027e5412ff540c10baaab108e5982
kevingermainbusiness <kevingermainbusiness@gmail.com>
2021-04-27 22:43:39 -0400

    Sixth commit

--- app/src/main/java/com/kevincodes/androidcanvas/MainActivity.kt
+++ app/src/main/java/com/kevincodes/androidcanvas/MainActivity.kt
@@ -15,19 +15,18 @@ import timber.log.Timber
 class MainActivity : AppCompatActivity() {
     companion object {
         private const val OFFSET = 120
-        private const val MULTIPLIER = 100
+        private const val COLOR_MULTIPLIER = 80
     }
 
     private lateinit var mCanvas: Canvas
     private var mDefaultPainter = Paint()
     private var mUnderlinedTextPainter = Paint(Paint.UNDERLINE_TEXT_FLAG)
     private lateinit var mBitmapOnImageView: Bitmap
-    private var mRect: Rect = Rect()
+    private var mInsideRect: Rect = Rect()
     private var mBounds: Rect = Rect()
     private var mOffset = OFFSET
     private var mFirstDrawnBackgroundColor = 0
     private var mRectFillColor = 0
-    private var mColorAccent = 0
     private var mWhiteColor = 0
 
     private lateinit var binding: ActivityMainBinding
@@ -41,7 +40,6 @@ class MainActivity : AppCompatActivity() {
     private fun initialize() {
         mFirstDrawnBackgroundColor = ResourcesCompat.getColor(resources, R.color.purple_500, null)
         mRectFillColor = ResourcesCompat.getColor(resources, R.color.purple_700, null)
-        mColorAccent = ResourcesCompat.getColor(resources, R.color.teal_200, null)
         mWhiteColor = ResourcesCompat.getColor(resources, R.color.white, null)
         // assign colors
         mDefaultPainter.color = mFirstDrawnBackgroundColor
@@ -49,6 +47,19 @@ class MainActivity : AppCompatActivity() {
         mUnderlinedTextPainter.textSize = 70f
     }
 
+    /**
+     * When a user just opened the app, the first condition will always be true,
+     * thus be executed first
+     * [mOffset] from what I can understand, helps adjusts the location of
+     * an element on the canvas, it's used as a pointer to place stuff on the canvas
+     * and define if a drawing should occur
+     *
+     * In the first condition, the imageView gets assigned its [Bitmap], the same
+     * [Bitmap] that the [mCanvas] will to draw into.
+     * The [mCanvas] draws a bg and a text on the [Bitmap], the [mOffset] is then
+     * incremented by 1, then [View.invalidate] is called to make these changes visible
+     * inside this [Bitmap]
+     * */
     fun respondToImageViewClick(view: View) {
         val vWidth = view.width
         val vHeight = view.height
@@ -61,7 +72,7 @@ class MainActivity : AppCompatActivity() {
             mCanvas.drawColor(mFirstDrawnBackgroundColor)
             mCanvas.drawText(
                 getString(R.string.keep_tapping),
-                (halfWidth * 0.5).toFloat(), // 50% of half width
+                (mOffset * 2).toFloat(), // Where the first rect will be drawn
                 (halfHeight * 0.15).toFloat(), // 15% of half height
                 mUnderlinedTextPainter
             )
@@ -70,29 +81,39 @@ class MainActivity : AppCompatActivity() {
         } else {
             if (mOffset < halfWidth && mOffset < halfHeight) {
                 // Change the color by subtracting an integer.
-                mDefaultPainter.color = mRectFillColor - MULTIPLIER * mOffset
-                mRect.set(
+                mDefaultPainter.color = mRectFillColor - COLOR_MULTIPLIER * mOffset
+                mInsideRect.set(
                     mOffset, mOffset, vWidth - mOffset, vHeight - mOffset
                 )
-                mCanvas.drawRect(mRect, mDefaultPainter)
-                // Increase the indent.
+                mCanvas.drawRect(mInsideRect, mDefaultPainter)
+                // Increase the offset.
                 mOffset += OFFSET
-                Timber.d("Offset size below half image size: $mOffset")
+                Timber.d("Half width: $halfWidth, Half Height:$halfHeight, Offset size below half image size: $mOffset")
             } else {
-                mDefaultPainter.color = mColorAccent
+                mDefaultPainter.color = mFirstDrawnBackgroundColor
                 mCanvas.drawCircle(
                     halfWidth.toFloat(),
                     halfHeight.toFloat(),
                     (halfWidth / 3).toFloat(),
                     mDefaultPainter
                 )
-                val text = getString(R.string.done)
+                val mTextInsideCircle = getString(R.string.done)
                 // Get bounding box for text to calculate where to draw it.
-                mUnderlinedTextPainter.getTextBounds(text, 0, text.length, mBounds)
+                mUnderlinedTextPainter.getTextBounds(
+                    mTextInsideCircle,
+                    0,
+                    mTextInsideCircle.length,
+                    mBounds
+                )
                 // Calculate x and y for text so it's centered.
                 val x = halfWidth - mBounds.centerX()
                 val y = halfHeight - mBounds.centerY()
-                mCanvas.drawText(text, x.toFloat(), y.toFloat(), mUnderlinedTextPainter)
+                mCanvas.drawText(
+                    mTextInsideCircle,
+                    x.toFloat(),
+                    y.toFloat(),
+                    mUnderlinedTextPainter
+                )
                 Timber.d("Offset size, not equal to original size nor inferior to half size: $mOffset")
             }
         }
